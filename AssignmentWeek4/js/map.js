114c1ae18eb9c849e55a1a836f549d961320be09
Alekhya <alekhyarvittalam@gmail.com>
2021-04-28 14:04:14 -0700

    working buttons

--- AssignmentWeek4/js/map.js
+++ AssignmentWeek4/js/map.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
 // create the map
 function createMap(lat,lon,zl){
 	map = L.map('map').setView([lat,lon], zl);
-​
+
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
@@ -34,13 +34,13 @@ function readCSV(path){
 			
 			// map the data
 			mapCSV(data);
-​
+
 		}
 	});
 }
-​
+
 function mapCSV(data){
-​
+
     let circleOptions = {
         radius: 5,
         weight: 1,
@@ -57,36 +57,36 @@ function mapCSV(data){
 		.on('mouseover',function(){
             this.openPopup()
         })
-​
+
 		//Sidebar is not clicking to show the marker
         markers.addLayer(marker)
 		
 		$('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${index})">${item.name_en}</div`)
-​
+
 		
 
     })
-​
+
 	markers.addTo(map);
-​
+
     map.fitBounds(markers.getBounds());
 }
-​
+
 	function flyToIndex(index){
 		// yk: the call to pan to the map to a given location is different from the loop
 		map.setZoom(12);
 		map.panTo(markers.getLayers()[index].getLatLng());
 		markers.getLayers()[index].openPopup()
 	}
-​
+
 	
 /*	Tried adding different layers but wasn't working
-​
+
 //Layer Groups
 var Cultural = new L.LayerGroup();
 var Natural = new L.LayerGroup();
 var Mixed = new L.LayerGroup();
-​
+
 //Markers
 function layer(marker){
 	if(marker.category == "Cultural"){
@@ -99,22 +99,22 @@ function layer(marker){
 		Mixed.addLayer(marker);
 	}
 }
-​
+
 let layers = {
 	"Cultural": Cultural,
 	"Natural": Natural,
 	"Mixed": Mixed
 }
-​
+
 // add layer control box
 L.control.layers(null,layers).addTo(map)
-​
-​
-​
+
+
+
 	
-​
+
 	function mapCSV(data){
-​
+
 		let circleOptions = {
 			radius: 5,
 			weight: 1,
@@ -129,16 +129,16 @@ L.control.layers(null,layers).addTo(map)
 					this.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`).openPopup()
 				})
 				Culturalmarkers.addLayer(Culturalmarker)
-​
+
 			}
-​
+
 			if(item.category == "Natural"){
 				letNaturalmarker = L.circleMarker([item.latitude,item.longitude], circleOptions).on('mouseover',function(){
 					this.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`).openPopup()
 				})
 				Naturalmarkers.addLayer(Naturalmarker)
 			}
-​
+
 			else{
 				letMixedmarker = L.circleMarker([item.latitude,item.longitude], circleOptions).on('mouseover',function(){
 					this.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`).openPopup()
@@ -155,17 +155,17 @@ L.control.layers(null,layers).addTo(map)
 			Mixedmarkers.addTo(map)
 	
 		})
-​
+
 		let addedLayers = {
 			"Cultural": Culturalmarkers,
 			"Natural": Naturalmarkers,
 			"Mixed": Mixedmarkers,
 		}
-​
+
 		L.control.layers(null,addedLayers).addTo(map);
-​
+
     //markers.addTo(map);
-​
+
     map.fitBounds(Culturalmarkers.getBounds());
 }
 */
