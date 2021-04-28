77ffc59c8fd4f9ccd122bf98e1a834fbb72a27c9
Alekhya <alekhyarvittalam@gmail.com>
2021-04-28 09:32:06 -0700

    changes to buttons

--- AssignmentWeek4/js/map.js
+++ AssignmentWeek4/js/map.js
@@ -8,22 +8,22 @@ let markers = L.featureGroup();
 //let Culturalmarkers = L.featureGroup();
 //let Naturalmarkers = L.featureGroup();
 //let Mixedmarkers = L.featureGroup();
-
+​
 // initialize
 $( document ).ready(function() {
     createMap(lat,lon,zl);
     readCSV(path);
 });
-
+​
 // create the map
 function createMap(lat,lon,zl){
 	map = L.map('map').setView([lat,lon], zl);
-
+​
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
-
+​
 // function to read csv data
 function readCSV(path){
 	Papa.parse(path, {
@@ -34,13 +34,13 @@ function readCSV(path){
 			
 			// map the data
 			mapCSV(data);
-
+​
 		}
 	});
 }
-
+​
 function mapCSV(data){
-
+​
     let circleOptions = {
         radius: 5,
         weight: 1,
@@ -50,37 +50,43 @@ function mapCSV(data){
     }
     //loop through each entry
     data.data.forEach(function(item,index){
-        let marker = L.circleMarker([item.latitude,item.longitude], circleOptions).on('mouseover',function(){
-            this.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`).openPopup()
+		// yk: I had to change the way the marker is created by adding the popup to the marker first,
+		// and then adding the mouseover effect
+		let marker = L.circleMarker([item.latitude,item.longitude], circleOptions)
+		.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`)
+		.on('mouseover',function(){
+            this.openPopup()
         })
-
+​
 		//Sidebar is not clicking to show the marker
         markers.addLayer(marker)
 		
 		$('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${index})">${item.name_en}</div`)
-
+​
 		
-
+​
     })
-
+​
 	markers.addTo(map);
-
+​
     map.fitBounds(markers.getBounds());
 }
-
+​
 	function flyToIndex(index){
-		map.flyTo([item.latitude,item.longitude],12)
+		// yk: the call to pan to the map to a given location is different from the loop
+		map.setZoom(12);
+		map.panTo(markers.getLayers()[index].getLatLng());
 		markers.getLayers()[index].openPopup()
 	}
-
+​
 	
 /*	Tried adding different layers but wasn't working
-
+​
 //Layer Groups
 var Cultural = new L.LayerGroup();
 var Natural = new L.LayerGroup();
 var Mixed = new L.LayerGroup();
-
+​
 //Markers
 function layer(marker){
 	if(marker.category == "Cultural"){
@@ -93,22 +99,22 @@ function layer(marker){
 		Mixed.addLayer(marker);
 	}
 }
-
+​
 let layers = {
 	"Cultural": Cultural,
 	"Natural": Natural,
 	"Mixed": Mixed
 }
-
+​
 // add layer control box
 L.control.layers(null,layers).addTo(map)
-
-
-
+​
+​
+​
 	
-
+​
 	function mapCSV(data){
-
+​
 		let circleOptions = {
 			radius: 5,
 			weight: 1,
@@ -123,16 +129,16 @@ L.control.layers(null,layers).addTo(map)
 					this.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`).openPopup()
 				})
 				Culturalmarkers.addLayer(Culturalmarker)
-
+​
 			}
-
+​
 			if(item.category == "Natural"){
 				letNaturalmarker = L.circleMarker([item.latitude,item.longitude], circleOptions).on('mouseover',function(){
 					this.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`).openPopup()
 				})
 				Naturalmarkers.addLayer(Naturalmarker)
 			}
-
+​
 			else{
 				letMixedmarker = L.circleMarker([item.latitude,item.longitude], circleOptions).on('mouseover',function(){
 					this.bindPopup(`<b>Name: </b>${item.name_en}<br><b>Description: </b>${item.short_description_en}`).openPopup()
@@ -149,17 +155,17 @@ L.control.layers(null,layers).addTo(map)
 			Mixedmarkers.addTo(map)
 	
 		})
-
+​
 		let addedLayers = {
 			"Cultural": Culturalmarkers,
 			"Natural": Naturalmarkers,
 			"Mixed": Mixedmarkers,
 		}
-
+​
 		L.control.layers(null,addedLayers).addTo(map);
-
+​
     //markers.addTo(map);
-
+​
     map.fitBounds(Culturalmarkers.getBounds());
 }
 */
